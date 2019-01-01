36ddeed7147522ed9b94cba5307ead25e9683989
dubilla <dan.ubilla@gmail.com>
2019-01-01 16:58:21 -0500

    Add RSpotify to pull playlists

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module SpotifyFarmSystem
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    RSpotify::authenticate(Rails.application.credentials.spotify[:client_id], Rails.application.credentials.spotify[:client_secret])
   end
 end
