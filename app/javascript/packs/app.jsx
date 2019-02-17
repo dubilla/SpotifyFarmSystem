0fb401eb5726a49bb2abdaba9351447eec79ede6
dubilla <dan.ubilla@gmail.com>
2019-02-17 16:10:32 -0500

    Display a list of playlists, rendered client-side

--- app/javascript/packs/app.jsx
+++ app/javascript/packs/app.jsx
@@ -2,9 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      playlists: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('/playlists')
+      .then(response => response.json())
+      .then(data => this.setState({ playlists: data }));
+  }
+
   render() {
+    const { playlists } = this.state
     return (
-      <div>Hi</div>
+      <section>
+        <h3>Playlists</h3>
+        <ul>
+          {playlists.map(playlist =>
+            <li key={playlist.id}>
+              {playlist.name}
+            </li>
+          )}
+        </ul>
+      </section>
     )
   }
 }
